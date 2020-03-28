d252dd4d6e08471103dc6d942ac65881324461b4
dudkinmn <dudkinmn@yandex.ru>
2020-03-28 23:22:39 +0300

    работа с временем и данными. оказалось потруднее немного

--- src/components/Process/Process.tsx
+++ src/components/Process/Process.tsx
@@ -12,15 +12,24 @@ import * as Icon from '../../img/icons'
 
 let Process = ({ process }: TProcessProps): ReactElement => {
 
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+
+  const processStart = new Date(Number(process.start))
+  const processEnd = new Date(Number(process.end))
+  const processLoading = new Date(Number(process.loading))
+
   const getHHMM = (msec: string): string => {
     const newDate = new Date(Number(msec))
-    const hh = (newDate.getHours() == 0) ? '' : `${newDate.getHours()} ч `
+    const hh = (newDate.getHours() && newDate.getDay() == 0) ? '' : `${(newDate.getDate()-1)*24 + newDate.getHours()} ч `
     const mm = (newDate.getMinutes() == 0) ? '' : `${newDate.getMinutes()} мин`
 
     return hh + mm
   }
 
-    console.log('process', process)
 
   return (
     <div className={styles.process} >
@@ -56,28 +65,29 @@ let Process = ({ process }: TProcessProps): ReactElement => {
             </div>
             <div className={styles.infoBlock}>
               <Icon.CountScenaries />
-              <span>{process.averageActiveTime}</span>
+              <span>{process.numberOfScenarios}</span>
               <p>в процессе</p>
             </div>
           </div>
           <div className={styles.infoColumn}>
             <div>
               <span>Начало</span>
-              <span>{process.start}</span>
+              <span>{processStart.toLocaleString("ru", options)}</span>
             </div>
             <div>
               <span>Окончание</span>
-              <span>{process.end}</span>
+              <span>{processEnd.toLocaleString("ru", options)}</span>
             </div>
             <div>
               <span>Загрузка</span>
-              <span>{process.loading}</span>
+              <span>{processLoading.toLocaleString("ru", options)}</span>
             </div>
           </div>
         </div>
     </div>
   );
 };
+
 
 
 export default Process;
